2cb47734835031ebe76252bca1042410f1054dfb
Thịnh <thjnh195@gmail.com>
2015-02-10 14:09:04 +0700

    Version 0.0.0.3

--- www/js/controllers/controllers-order.js
+++ www/js/controllers/controllers-order.js
@@ -1,6 +1,8 @@
 angular.module('MainApp.controllers.order', [])
 
-	.controller('OrderController', function ($scope, MenuService) {
+	.controller('OrderController', function ($scope,$ionicModal, MenuService) {
+        
+
         $scope.navTitle = MenuService.get(1).text;
 
         $scope.leftButtons = [{
@@ -15,4 +17,22 @@ angular.module('MainApp.controllers.order', [])
                 $scope.sideMenuController.toggleRight();
             }
         }];
+
+
+        $ionicModal.fromTemplateUrl('templates/modal.html', function($ionicModal) {
+            $scope.modal = $ionicModal;}, {
+            // Use our scope for the scope of the modal to keep it simple
+            scope: $scope,
+            // The animation we want to use for the modal entrance
+            animation: 'slide-in-up'
+        });
+
+      
+        $scope.openModal = function() {
+            console.log('Opening Modal');
+            $scope.modal.show();
+        };
+        $scope.closeModal = function(){
+            $scope.modal.hide();
+        }
     });
