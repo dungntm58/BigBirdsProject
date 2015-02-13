eaae38268403108adbc2003205a11d0d34fc4b9b
BigBirdsStudio <bigbirdsstudio@gmail.com>
2015-02-13 09:15:58 +0700

    v0.0.0.42112

--- www/js/controllers/controllers-order.js
+++ www/js/controllers/controllers-order.js
@@ -40,47 +40,57 @@ var orderControllers = angular.module('MainApp.controllers.order', [])
         $scope.MainCourses = MenuFoodService.mainCourses();
         $scope.Desserts = MenuFoodService.desserts();
         $scope.Drinks = MenuFoodService.drinks();
-
         $scope.Currency = '$';
-        $scope.dishes = [];
-        $scope.quantity = [];
+
+        $scope.order = {
+            dishes: [],
+            quantity: [],
+            table: null,
+            date: null
+        };
 
         $scope.selectDish = function(dish){
-            var index = $scope.dishes.indexOf(dish);
+            var index = $scope.order.dishes.indexOf(dish);
+
             if (index >= 0){
-                $scope.quantity[index]++;
+                $scope.order.quantity[index]++;
             }
             else{
-                $scope.dishes.push(dish);
-                $scope.quantity.push(1);
+                $scope.order.dishes.push(dish);
+                $scope.order.quantity.push(1);
             }
         };
 
         $scope.removeDish = function(dish){
-            var index = $scope.dishes.indexOf(dish);
-            $scope.dishes.splice(index, 1);
-            $scope.quantity.splice(index, 1);
+            var index = $scope.order.dishes.indexOf(dish);
+            $scope.order.dishes.splice(index, 1);
+            $scope.order.quantity.splice(index, 1);
         };
 
         $scope.increaseQuantity = function(dish){
-            var index = $scope.dishes.indexOf(dish);
-            $scope.quantity[index]++;
+            var index = $scope.order.dishes.indexOf(dish);
+            $scope.order.quantity[index]++;
         };
 
         $scope.valueOfOrder = function(){
             var total = 0;
-            for (var i = 0; i<$scope.dishes.length ; i++){
-                total += parseFloat($scope.dishes[i].price)*parseInt($scope.quantity[i]);
+            for (var i = 0; i<$scope.order.dishes.length ; i++){
+                total += parseFloat($scope.order.dishes[i].price)*parseInt($scope.order.quantity[i]);
             }
             return total;
         }
 
         $scope.decreaseQuantity = function(dish){
-            var index = $scope.dishes.indexOf(dish);
-            if ($scope.quantity[index] > 0){
-                $scope.quantity[index]--;
-            }
-            if ($scope.quantity[index] == 0)
+            var index = $scope.order.dishes.indexOf(dish);
+            $scope.order.quantity[index]--;
+            if ($scope.order.quantity[index] == 0)
                 $scope.removeDish(dish);
+        };
+
+        $scope.resetOrder = function(){
+            $scope.order.dishes = [];
+            $scope.order.quantity = [];
+            $scope.order.table = null;
+            $scope.order.date = null;
         };
     });
