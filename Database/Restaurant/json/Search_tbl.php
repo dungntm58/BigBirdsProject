b8892a38567a0c891d08d2c9b02c86ecbff6cbcb
Nguyen Manh Hung <hungnm.uet@gmail.com>
2015-05-11 01:09:44 +0700

    a

--- Database/Restaurant/json/Search_tbl.php
+++ Database/Restaurant/json/Search_tbl.php
@@ -25,7 +25,89 @@
 		$date2 = date('Y-m-d',$date1);
 		//echo $date2;
 		
-		$hour = substr($day,11, 8);
+		<?php
+	
+	error_reporting(E_NOTICE || E_WARNING);
+	
+	$string = file_get_contents("test.json");
+	$json = json_decode($string,true);//chuyển $json thành mảng
+	
+	//$user_id = array(); // khai báo mảng user_id chưa các id
+	
+	foreach($json as $key => $value) {
+		if (is_array($value)) {
+			foreach ($value as $key => $value) {
+				if($key =='resID'){
+					$id= $value;
+					//echo $user_id."<br />";
+				}
+				else{
+					$day = $value;
+				}
+			}
+		}
+    }
+		$date = substr($day, 0, 10);
+		$hour = (int)substr($day,11, 2);
+		$min = (int)substr($day,14,2);
+		$sec = (int)substr($day,16,2);
+		$time = $hour*3600 + $min*60 + $sec;
+		$time_1 = $time + 7200;
+		if($time >=7200){
+			$time_2 = $time - 7200;
+		}
+		echo $time;
+		
+		
+	
+	$db_user = 'root'; //User đăng nhập MYSQL
+	$db_pass = ''; // Pass đăng nhập MySQL
+	$db_host = 'localhost'; //IP, Domain kết nối
+	$db_name = 'restaurant_db'; //Tên CSDL
+	
+	
+	//Tạo biến kết nối với CSDL
+	$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die('Kết nối thất bại');
+
+	//Lấy kiểu định dạng muốn lấy của request
+	$formatList = array('json', 'xml');
+	
+	if (isset($_GET['format'])) {
+		$format = $_GET['format'];
+	} else {
+		$format = 'json';
+	}
+	if (!in_array($format, $formatList)) {
+		$format = 'json';
+	}
+
+	//Truy vấn
+	$query = mysqli_query($conn, "SELECT `table_id`,`table_content` FROM `table` WHERE table_id NOT IN (SELECT `or_table` FROM `order` WHERE order.or_table = table.table_id AND 
+	or_date = '$date' AND or_time > '$time_2' AND or_time < '$time_1')");
+	
+	//var_dump($query);
+	
+	
+	if (!$query) {
+		printf("Error: %s\n", mysqli_error($conn));
+		exit();
+	}
+
+	//Tạo bảng lưu thông tin
+	$users = array();
+	while ($user_ar = mysqli_fetch_assoc($query)) {
+		$users[] = $user_ar;
+	}
+
+	//Trả về kiểu json
+	if ($format == 'json') {
+		header('Content-type: application/json; charset=utf-8');
+		echo json_encode($users);
+	}
+
+
+	mysqli_close($conn);
+?>
 		$hour1 = strtotime($hour);
 		$mysql = date( 'H:i:s', $hour1 );
 		//echo $mysql;
@@ -60,7 +142,7 @@
 	}
 
 	//Truy vấn
-	$query = mysqli_query($conn, "SELECT `table_id`,`table_content` FROM `table` WHERE table_id NOT IN (SELECT `or_table` FROM `order` WHERE order.or_table = table.table_id AND or_date = '$date2' AND or_time < '$mysql1' AND or_time > '$mysql2')");
+	$query = mysqli_query($conn, "SELECT `table_id`,`table_content` FROM `table` WHERE table_id NOT IN (SELECT `or_table` FROM `order` WHERE order.or_table = table.table_id AND or_date = STR_TO_DATE($date2, '%d/%m/%Y') AND or_time < '$mysql1' AND or_time > '$mysql2')");
 	
 	//var_dump($query);
 	
--- Database/Restaurant/restaurant_db.sql
+++ Database/Restaurant/restaurant_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2015 at 06:12 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: May 10, 2015 at 08:07 PM
+-- Server version: 5.5.36
+-- PHP Version: 5.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `catalog` (
   `ctl_id` int(10) NOT NULL AUTO_INCREMENT,
   `ctl_name` varchar(255) NOT NULL,
-  `ctl_content` text NOT NULL,
   `user_id` int(10) NOT NULL,
+  `ctl_content` varchar(255) NOT NULL,
   PRIMARY KEY (`ctl_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
@@ -38,11 +38,11 @@ CREATE TABLE IF NOT EXISTS `catalog` (
 -- Dumping data for table `catalog`
 --
 
-INSERT INTO `catalog` (`ctl_id`, `ctl_name`, `ctl_content`, `user_id`) VALUES
-(1, 'drink', 'vodka', 2),
-(2, 'desert', 'nothing', 2),
-(3, 'aaaaaaaaa', 'sdfsdf', 2),
-(4, 'fdsgdfsgsd', 'sdfgsfdg', 2);
+INSERT INTO `catalog` (`ctl_id`, `ctl_name`, `user_id`, `ctl_content`) VALUES
+(1, 'drink', 2, ''),
+(2, 'desert', 2, ''),
+(3, 'aaaaaaaaa', 2, ''),
+(4, 'fdsgdfsgsd', 2, '');
 
 -- --------------------------------------------------------
 
@@ -52,11 +52,11 @@ INSERT INTO `catalog` (`ctl_id`, `ctl_name`, `ctl_content`, `user_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `order` (
   `or_id` int(10) NOT NULL AUTO_INCREMENT,
-  `or_time` time DEFAULT NULL,
   `or_list` varchar(255) NOT NULL,
   `or_amount` double NOT NULL,
   `or_table` int(10) NOT NULL,
-  `or_date` date NOT NULL,
+  `or_date` varchar(255) NOT NULL,
+  `or_time` bigint(20) NOT NULL,
   PRIMARY KEY (`or_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
@@ -64,9 +64,9 @@ CREATE TABLE IF NOT EXISTS `order` (
 -- Dumping data for table `order`
 --
 
-INSERT INTO `order` (`or_id`, `or_time`, `or_list`, `or_amount`, `or_table`, `or_date`) VALUES
-(1, '10:10:00', 'adsfasdfasf', 12345, 1, '2015-05-11'),
-(2, '20:00:00', 'ádfasf', 123123, 2, '2015-05-12');
+INSERT INTO `order` (`or_id`, `or_list`, `or_amount`, `or_table`, `or_date`, `or_time`) VALUES
+(1, 'adsfasdfasf', 12345, 1, '2015-05-03', 600),
+(2, 'ádfasf', 123123, 2, '0000-00-00', 800);
 
 -- --------------------------------------------------------
 
@@ -79,9 +79,9 @@ CREATE TABLE IF NOT EXISTS `product` (
   `pro_name` varchar(255) NOT NULL,
   `pro_price` int(20) NOT NULL,
   `pro_image` varchar(255) NOT NULL,
-  `pro_description` text NOT NULL,
   `user_id` int(10) NOT NULL,
   `ctl_id` int(10) NOT NULL,
+  `pro_descripttion` varchar(255) NOT NULL,
   PRIMARY KEY (`pro_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
@@ -89,10 +89,10 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`pro_id`, `pro_name`, `pro_price`, `pro_image`, `pro_description`, `user_id`, `ctl_id`) VALUES
-(1, 'aaa', 12, 'ádfdsa', 'sadfdsaf', 1, 1),
-(2, 'sdfsdf', 23, 'adsfasdf', 'aasdf', 1, 2),
-(3, 'ểtrt', 123, 'ểtrt', 'ẻtret', 1, 3);
+INSERT INTO `product` (`pro_id`, `pro_name`, `pro_price`, `pro_image`, `user_id`, `ctl_id`, `pro_descripttion`) VALUES
+(1, 'aaa', 12, 'ádfdsa', 1, 1, ''),
+(2, 'sdfsdf', 23, 'adsfasdf', 1, 2, ''),
+(3, 'ểtrt', 123, 'ểtrt', 1, 2, '');
 
 -- --------------------------------------------------------
 
@@ -140,7 +140,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`user_id`, `user_email`, `user_pass`, `user_level`, `user_image`, `user_add`, `user_name`, `restaurant_name`) VALUES
 (1, 'hung1@gmail.com', 'hunghung1', 1, '', '', '', ''),
-(2, 'hung2@gmail.com', 'hunghung2', 2, '', 'cho lon', '', 'AAAAAAAAAAAAAA'),
+(2, 'hung2@gmail.com', 'hunghung2', 1, '', '', '', ''),
 (3, 'hung11', 'hung1', 2, 'khong co gi', 'xuan thuy, cau giay,ha noi', '', ''),
 (4, '', '', 1, 'aaaaa', 'duong quang ham, cau giay', '', ''),
 (5, '', '', 2, '123kjlk.jpg', 'le duc tho keo dai', '', ''),
