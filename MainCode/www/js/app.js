9e328c941f065ffcd38ed1f73ff88a007c0d0775
sonla58 <leanhson.9x@gmail.com>
2015-05-13 00:28:01 +0700

    Change GUI sign in page and side-menu

--- MainCode/www/js/app.js
+++ MainCode/www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('MainApp', ['ionic', 'MainApp.controllers', 'MainApp.directives', 'MainApp.services', 'ngCordova', 'ngCookies'])
+angular.module('MainApp', ['ionic', 'MainApp.controllers', 'MainApp.directives', 'MainApp.services', 'ngCordova', 'ngCookies','ionic.contrib.drawer'])
     .run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
